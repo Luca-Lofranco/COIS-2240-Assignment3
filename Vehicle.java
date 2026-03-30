6b7e5282223fc1299ee96e52323d19a5d3138161
Luca-Lofranco <lucalofranco@trentu.ca>
2026-03-29 21:56:00 -0400

    added capitalize methods and adjusted vehicle constructor

--- Vehicle.java
+++ Vehicle.java
@@ -8,16 +8,8 @@ public abstract class Vehicle {
     public enum VehicleStatus { Available, Held, Rented, UnderMaintenance, OutOfService }
 
     public Vehicle(String make, String model, int year) {
-    	if (make == null || make.isEmpty())
-    		this.make = null;
-    	else
-    		this.make = make.substring(0, 1).toUpperCase() + make.substring(1).toLowerCase();
-    	
-    	if (model == null || model.isEmpty())
-    		this.model = null;
-    	else
-    		this.model = model.substring(0, 1).toUpperCase() + model.substring(1).toLowerCase();
-    	
+    	this.make = capitalize(make);
+        this.model = capitalize(model);
         this.year = year;
         this.status = VehicleStatus.Available;
         this.licensePlate = null;
@@ -47,6 +39,10 @@ public abstract class Vehicle {
 
     public String getInfo() {
         return "| " + licensePlate + " | " + make + " | " + model + " | " + year + " | " + status + " |";
+    }
+    
+    private String capitalize(String input) {
+        return Character.toUpperCase(input.charAt(0)) + input.substring(1);
     }
 
 }
